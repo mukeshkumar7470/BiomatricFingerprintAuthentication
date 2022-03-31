d195e65a62f42dc585fda7f7cc948bc6304d2230
Mukesh kumar Patel <kumarmukeshpatel57@gmail.com>
2022-03-31 11:54:44 +0530

    Initial commit

---
+++ app/src/main/java/com/mukeshkpdeveloper/biomatricfingerprintauthentication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.mukeshkpdeveloper.biomatricfingerprintauthentication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mukeshkpdeveloper.biomatricfingerprintauthentication.databinding.ActivityMainBinding;
+
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        initlizeBiometric();
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricDialog();
+            }
+        });
+
+
+    }
+
+    private void biometricDialog() {
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        // this will give us result of AUTHENTICATION
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            // THIS METHOD IS CALLED WHEN AUTHENTICATION IS SUCCESS
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                binding.login.setText("Login Successful");
+            }
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Test")
+                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
+
+        biometricPrompt.authenticate(promptInfo);
+
+    }
+
+    private void initlizeBiometric() {
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK | BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                binding.msgtext.setText("You can use the fingerprint sensor to login");
+                binding.msgtext.setTextColor(Color.parseColor("#000000"));
+                binding.login.setVisibility(View.VISIBLE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Log.e("MY_APP_TAG", "This device does not have a fingerprint sensor.");
+                binding.msgtext.setText("This device does not have a fingerprint sensor.");
+                binding.login.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.");
+                binding.msgtext.setText("The biometric sensor is currently unavailable");
+                binding.login.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                // Prompts the user to create credentials that your app accepts.
+                binding.msgtext.setText("Your device doesn't have fingerprint saved,please check your security settings");
+                binding.login.setVisibility(View.GONE);
+                break;
+        }
+    }
+}
